98a51a26f961c44fd340aee238b5733af889c9ce
Jose Ruiz <joeruizl87@gmail.com>
2023-08-03 14:23:16 -0400

    update

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const tasks = require("./routes/task")
+const connectDB = require('./db/connect')
+
+require('dotenv').config()
 
 //middleware
 
@@ -16,4 +19,14 @@ app.use("/api/v1/task", tasks)
 
 const port = 8000
 
-app.listen(port, console.log(`Listening in port ${port}...`))
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Listening in port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,1 +1,12 @@
 // const connectionString=
+
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+    return
+    mongoose.connect(url)
+        .then(() => { console.log('connect to DB'); })
+        .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
